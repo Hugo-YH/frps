2d7bbf085d5a86c59a127f50801c133556f6bd45
Hugo-YH <8456755+Hugo-YH@users.noreply.github.com>
2025-06-21 23:20:02 +0800

    添加注释

--- frps_linux_install.sh
+++ frps_linux_install.sh
@@ -2,16 +2,16 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-# fonts color
+# 字体颜色定义
 Green="\033[32m"
 Red="\033[31m"
 Yellow="\033[33m"
 GreenBG="\033[42;37m"
 RedBG="\033[41;37m"
 Font="\033[0m"
-# fonts color
+# 字体颜色定义结束
 
-# variable
+# 变量定义
 FRP_VERSION=0.62.1
 REPO=stilleshan/frps
 WORK_PATH=$(dirname $(readlink -f $0))
@@ -19,7 +19,7 @@ FRP_NAME=frps
 FRP_PATH=/usr/local/frp
 PROXY_URL="https://ghfast.top/"
 
-# check frps
+# 检查是否已安装frps
 if [ -f "/usr/local/frp/${FRP_NAME}" ] || [ -f "/usr/local/frp/${FRP_NAME}.toml" ] || [ -f "/lib/systemd/system/${FRP_NAME}.service" ];then
     echo -e "${Green}=========================================================================${Font}"
     echo -e "${RedBG}当前已退出脚本.${Font}"
@@ -33,12 +33,13 @@ if [ -f "/usr/local/frp/${FRP_NAME}" ] || [ -f "/usr/local/frp/${FRP_NAME}.toml"
     exit 2
 fi
 
+# 检查并杀死正在运行的frps进程
 while ! test -z "$(ps -A | grep -w ${FRP_NAME})"; do
     FRPSPID=$(ps -A | grep -w ${FRP_NAME} | awk 'NR==1 {print $1}')
     kill -9 $FRPSPID
 done
 
-# check pkg
+# 检查并安装wget和curl
 if type apt-get >/dev/null 2>&1 ; then
     if ! type wget >/dev/null 2>&1 ; then
         apt-get install wget -y
@@ -57,11 +58,11 @@ if type yum >/dev/null 2>&1 ; then
     fi
 fi
 
-# check network
+# 检查网络连通性
 GOOGLE_HTTP_CODE=$(curl -o /dev/null --connect-timeout 5 --max-time 8 -s --head -w "%{http_code}" "https://www.google.com")
 PROXY_HTTP_CODE=$(curl -o /dev/null --connect-timeout 5 --max-time 8 -s --head -w "%{http_code}" "${PROXY_URL}")
 
-# check arch
+# 检查系统架构
 if [ $(uname -m) = "x86_64" ]; then
     PLATFORM=amd64
 fi
@@ -72,6 +73,7 @@ fi
 
 FILE_NAME=frp_${FRP_VERSION}_linux_${PLATFORM}
 
+# 下载frps及配置文件
 if [ ! -f "${WORK_PATH}/${FILE_NAME}.tar.gz" ]; then
     if [ $GOOGLE_HTTP_CODE == "200" ]; then
         wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
@@ -90,12 +92,13 @@ else
     echo -e "${Green}文件 ${FILE_NAME}.tar.gz 已存在, 跳过下载.${Font}"
 fi
 
+# 解压并移动文件到指定目录
 tar -zxvf ${FILE_NAME}.tar.gz
 mkdir -p ${FRP_PATH}
 mv ${FILE_NAME}/${FRP_NAME} ${FRP_PATH}
 mv ${FRP_NAME}.toml ${FRP_PATH}
 
-# 配置 frps.service
+# 配置 systemd 服务
 cat >/lib/systemd/system/frps.service <<'EOF'
 [Unit]
 Description=Frp Server Service
@@ -113,13 +116,15 @@ WantedBy=multi-user.target
 
 EOF
 
+# 重新加载 systemd 配置并启动服务
 systemctl daemon-reload
 sudo systemctl start ${FRP_NAME}
 sudo systemctl enable ${FRP_NAME}
 
-# clean
+# 清理安装文件
 rm -rf ${WORK_PATH}/${FILE_NAME}.tar.gz ${WORK_PATH}/${FILE_NAME} ${FRP_NAME}_linux_install.sh
 
+# 输出安装成功提示
 echo -e "${Green}====================================================================${Font}"
 echo -e "${Green}安装成功,请先修改 ${FRP_NAME}.toml 文件,确保格式及配置正确无误!${Font}"
 echo -e "${Red}vi /usr/local/frp/${FRP_NAME}.toml${Font}"
